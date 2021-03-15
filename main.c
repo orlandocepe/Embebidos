75fd35b4139abcb98617c0d2d41d9a793a9ee212
orlando <orlandocepeda1998@gmail.com>
2021-03-15 12:46:21 -0500

    Funcion suma

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+int suma(int a, int b){
+return(a+b);
+}
 int main(){
-printf("Sistemas Embebidos\n");
+printf("La suma de 5 mas 2 es= %d\n",suma(5,2));
 return(0);
 }
 
